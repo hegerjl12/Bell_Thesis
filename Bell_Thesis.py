18f5a16d987f6a9af0fcab89db365f79b2dce416
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-08 11:29:02 -0800

    ft

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -14,7 +14,9 @@ image = Image.open('404A4573.jpg')
 
 st.image(image, width=1024)
 
-image1_input1 = st.text_input('Enter 5 words you feel:', '', key=1)
+image1_inputs = []
+
+image1_input.append(st.text_input('Enter 5 words you feel:', '', key=1))
 image1_input2 = st.text_input('','', key=2)
 image1_input3 = st.text_input('','', key=3)
 image1_input4 = st.text_input('','', key=4)
