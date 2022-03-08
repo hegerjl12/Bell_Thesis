d95d9bcaa8dd0ba2218bfe303ce523ea2e5b270b
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-08 11:40:00 -0800

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -13,13 +13,12 @@ st.title("Selina's Thesis")
 image = Image.open('404A4573.jpg')
 
 st.image(image, width=1024)
-N = 3
-image1_inputs[N] = []
 
-for i in range(N):
-    image1_inputs[i] = st.text_inputs('Enter 5 words you feel:', '', key=1))
-    
+image1_input1 = st.text_input('Enter 5 words you feel:', '', key=1))    
 image1_input2 = st.text_input('','', key=2)
 image1_input3 = st.text_input('','', key=3)
 image1_input4 = st.text_input('','', key=4)
 image1_input5 = st.text_input('','', key=5)
+
+if not image1_input1:
+    image1_input1 = st.text_input('Enter 5 words you feel:', image1_input1, key=1, disabled=True)
