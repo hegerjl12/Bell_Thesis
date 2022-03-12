2ae083f48c39d665a04432d11fd40cffe39f2d24
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-12 12:40:26 -0800

    button

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -14,10 +14,15 @@ image = Image.open('404A4573.jpg')
 
 st.image(image, width=1024)
 
-image1_input1 = st.text_input('Enter 5 words you feel:', '', key=1)  
+st.write('Enter 5 words you feel:')
+
+image1_input1 = st.text_input('','', key=1)  
 image1_input2 = st.text_input('','', key=2)
 image1_input3 = st.text_input('','', key=3)
 image1_input4 = st.text_input('','', key=4)
 image1_input5 = st.text_input('','', key=5)
 
-submit = st.button('Submit', key=6)
+submit = st.button('Submit', key=6, disabled=True)
+
+if image1_input1 and image1_input2 and image1_input3 and image1_input4 and image1_input5:
+     submit(disabled=False)
