7a3f366e72a8b841dc68b682af84bf35816a0926
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-12 19:01:37 -0800

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -52,7 +52,7 @@ image_input5 = st.text_input('','', key=5)
 submit = False
 
 if image_input1 and image_input2 and image_input3 and image_input4 and image_input5:
-     submit = st.button('Submit', disabled=False)
+     submit = st.button('Submit', key=16, disabled=False)
 
 if submit:
      df = df.append({'Image1' : image_input1}, ignore_index = True)
@@ -77,7 +77,7 @@ image_input5 = st.text_input('','', key=10)
 submit = False
 
 if image_input1 and image_input2 and image_input3 and image_input4 and image_input5:
-     submit = st.button('Submit', disabled=False)
+     submit = st.button('Submit', key=17, disabled=False)
 
 if submit:
      df = df.append({'Image2' : image_input1}, ignore_index = True)
@@ -101,7 +101,7 @@ image_input5 = st.text_input('','', key=15)
 submit = False
 
 if image_input1 and image_input2 and image_input3 and image_input4 and image_input5:
-     submit = st.button('Submit', disabled=False)
+     submit = st.button('Submit', key=18, disabled=False)
 
 if submit:
      df = df.append({'Image3' : image_input1}, ignore_index = True)
