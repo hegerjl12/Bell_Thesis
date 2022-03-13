0a14fa05622ecd01bf7dc780ee6797a03ec3e33d
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-12 21:14:08 -0800

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -3,7 +3,7 @@ import pandas as pd
 from PIL import Image
 import random
 
-#@st.cache
+@st.cache
 def run_image_order(image_order):
      while len(image_order) != 3:
           num = random.randint(1,3)
@@ -23,84 +23,27 @@ st.set_page_config(
 
 st.title("Selina's Thesis")
 
-df = pd.DataFrame(columns = ['Image1', 'Image2', 'Image3', 'Image4', 'Image5'])
+df = pd.DataFrame(columns = ['Image1', 'Image2', 'Image3', 'Image4', 'Image5', 'Image6', 'Image7', 'Image8', 'Image9', 'Image10'])
 
 image_order = []
 
 image_order = run_image_order(image_order)
 
-image = Image.open('image' + str(image_order[0]) + '.jpg')
+for i in image_order:
+     match i:
+          case 1:
+               image = Image.open('image1.jpg')
 
-st.image(image, width=1024)
+               st.image(image, width=1024)
 
-st.write('Enter 5 words you feel:')
+               st.write('Enter 3 words you feel:')
 
-image_input1 = st.text_input('','', key=1)  
-image_input2 = st.text_input('','', key=2)
-image_input3 = st.text_input('','', key=3)
-image_input4 = st.text_input('','', key=4)
-image_input5 = st.text_input('','', key=5)
+               image_input1 = st.text_area('','', key=1)  
 
-submit1 = False
+               submit1 = st.button('Submit', key=16, disabled=False)
 
-if image_input1 and image_input2 and image_input3 and image_input4 and image_input5:
-     submit = st.button('Submit', key=16, disabled=False)
-
-if submit1:
-     df = df.append({'Image1' : image_input1}, ignore_index = True)
-     df = df.append({'Image1' : image_input2}, ignore_index = True)
-     df = df.append({'Image1' : image_input3}, ignore_index = True)
-     df = df.append({'Image1' : image_input4}, ignore_index = True)
-     df = df.append({'Image1' : image_input5}, ignore_index = True)
-
-
-image = Image.open('image' + str(image_order[1]) + '.jpg')
-
-st.image(image, width=1024)
-
-st.write('Enter 5 words you feel:')
-
-image_input1 = st.text_input('','', key=6)  
-image_input2 = st.text_input('','', key=7)
-image_input3 = st.text_input('','', key=8)
-image_input4 = st.text_input('','', key=9)
-image_input5 = st.text_input('','', key=10)
-
-submit2 = False
-
-if image_input1 and image_input2 and image_input3 and image_input4 and image_input5:
-     submit2 = st.button('Submit', key=17, disabled=False)
-
-if submit2:
-     df = df.append({'Image2' : image_input1}, ignore_index = True)
-     df = df.append({'Image2' : image_input2}, ignore_index = True)
-     df = df.append({'Image2' : image_input3}, ignore_index = True)
-     df = df.append({'Image2' : image_input4}, ignore_index = True)
-     df = df.append({'Image2' : image_input5}, ignore_index = True)
-
-image = Image.open('image' + str(image_order[2]) + '.jpg')
-
-st.image(image, width=1024)
-
-st.write('Enter 5 words you feel:')
-
-image_input1 = st.text_input('','', key=11)  
-image_input2 = st.text_input('','', key=12)
-image_input3 = st.text_input('','', key=13)
-image_input4 = st.text_input('','', key=14)
-image_input5 = st.text_input('','', key=15)
-
-submit3 = False
-
-if image_input1 and image_input2 and image_input3 and image_input4 and image_input5:
-     submit3 = st.button('Submit', key=18, disabled=False)
-
-if submit3:
-     df = df.append({'Image3' : image_input1}, ignore_index = True)
-     df = df.append({'Image3' : image_input2}, ignore_index = True)
-     df = df.append({'Image3' : image_input3}, ignore_index = True)
-     df = df.append({'Image3' : image_input4}, ignore_index = True)
-     df = df.append({'Image3' : image_input5}, ignore_index = True)
-
-if submit1 and submit2 and submit3:
-     st.write(df)
+               if submit1:
+                    words = image_input1.split()
+                    df = df.append({('Image1' : words[0]}, ignore_index = True)
+                    df = df.append({('Image1' : words[1]}, ignore_index = True)
+                    df = df.append({('Image1' : words[2]}, ignore_index = True)
