175ed71ca3796dc8c1e750d9025acc7088228d37
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-12 19:16:28 -0800

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -5,7 +5,7 @@ import numpy as np
 
 @st.cache
 def run_image_order(image_order):
-     image_order = np.random.randit(1,3,3)
+     image_order = np.random.randit(3, size=3)
      
      return image_order
 
