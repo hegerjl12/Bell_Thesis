8f523165d4b7b32d75809a7e271859379c3396f6
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-14 16:17:23 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -18,7 +18,9 @@ st.title("Selina's Thesis")
 
 df = pd.DataFrame(columns = ['Image1', 'Image2', 'Image3', 'Image4', 'Image5', 'Image6', 'Image7', 'Image8', 'Image9', 'Image10'])
 
-#if image_order not in st.sessionstate
+if image_order not in st.sessionstate:
+     image_order = []
+     
 i = random.randint(1,3)
 
 while i in image_order:
