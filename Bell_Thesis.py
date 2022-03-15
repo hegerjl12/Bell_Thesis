9681298d7ab5584b36d2f7ccd53a5acd93b09245
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-14 18:18:33 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -13,6 +13,7 @@ st.set_page_config(
 
 st.title("Selina's Thesis")
 arr = np.arange(3)
+st.write(arr)
 
 if 'df' not in st.session_state:
      st.session_state.df = pd.DataFrame(columns = ['Image1', 'Image2', 'Image3', 'Image4', 'Image5', 'Image6', 'Image7', 'Image8', 'Image9', 'Image10'])
