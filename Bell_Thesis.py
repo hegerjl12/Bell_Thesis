2e7b61e5922b9d7997428c4dec48778eae794963
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-14 17:23:20 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -17,10 +17,13 @@ if 'df' not in st.session_state:
      st.session_state.df = pd.DataFrame(columns = ['Image1', 'Image2', 'Image3', 'Image4', 'Image5', 'Image6', 'Image7', 'Image8', 'Image9', 'Image10'])
 
 if 'image_order' not in st.session_state:
-     st.session_state.image_order = []
+     st.session_state.image_order = np.random.randint(3, size=3)
      
 if 'i' not in st.session_state:
-     st.session_state.i = np.random.randint(3, size=3)
+     st.session_state.i = st.session_state.image_order[0]
+
+if 'next' not in st.session_state:
+     st.session_state.next = 0
 
 
 st.write(st.session_state.image_order)
@@ -41,10 +44,7 @@ if submit:
      st.session_state.df = st.session_state.df.append({('Image'+str(st.session_state.i)): words[1]}, ignore_index = True)
      st.session_state.df = st.session_state.df.append({('Image'+str(st.session_state.i)): words[2]}, ignore_index = True)
 
-     while st.session_state.i in st.session_state.image_order:
-          st.session_state.i = random.randint(1,3)
-          
-     st.session_state.image_order.append(st.session_state.i)
-     st.session_state.i = random.randint(1,3)
-
      st.write(st.session_state.df)
+
+     st.session_state.i = st.session_state.image_order[st.session_state.next+1]
+     
