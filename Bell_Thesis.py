949e5eaac449cb0f7d7ffa8e0bff21008726502e
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-14 17:34:17 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -17,9 +17,9 @@ if 'df' not in st.session_state:
      st.session_state.df = pd.DataFrame(columns = ['Image1', 'Image2', 'Image3', 'Image4', 'Image5', 'Image6', 'Image7', 'Image8', 'Image9', 'Image10'])
 
 if 'image_order' not in st.session_state:
-<<<<<<< HEAD
+
      st.session_state.image_order = np.random.shuffle(np.random.randint(3, size=3))
-=======
+
      
 if 'i' not in st.session_state:
      st.session_state.i = st.session_state.image_order[1]
