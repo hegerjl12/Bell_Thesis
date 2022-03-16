bc575b4178e445b59b4f2138fc92e2f6390aa703
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-16 10:32:37 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -18,7 +18,7 @@ def set_image():
 
 st.title("Selina's Thesis")
 
-image_container = st.container()
+image_container = st.empty()
 
 if 'df' not in st.session_state:
      st.session_state.df = pd.DataFrame(columns = ['Image1', 'Image2', 'Image3', 'Image4', 'Image5', 'Image6', 'Image7', 'Image8', 'Image9', 'Image10'])
