7910f53473a49a4ce585e2cdad646909cf286e1d
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-28 09:43:38 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -25,7 +25,8 @@ def run_query(query):
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-st.write(rows)
+for row in rows:
+     st.write(row)
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
