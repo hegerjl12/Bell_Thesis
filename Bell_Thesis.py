5bf6f407345165413a83d6391094984e4f64706a
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-29 11:06:21 -0700

    test other

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -11,6 +11,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import os.path
+import pickle
 
 
 st.set_page_config(
@@ -27,18 +28,18 @@ creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-if os.path.exists('token.json'):
-     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-     if not creds or not creds.valid:
-          if creds and creds.expired and creds.refresh_token:
-               creds.refresh(Request())
-          else:
-               flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-               creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-     with open('token.json', 'w') as token:
-          token.write(creds.to_json())
+if os.path.exists('token.pickle'):
+     with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+if not creds or not creds.valid:
+     if creds and creds.expired and creds.refresh_token:
+          creds.refresh(Request())
+     else:
+          flow = InstalledAppFlow.from_client_secrets_file(
+          'token.json', SCOPES) # here enter the name of your downloaded JSON file
+          creds = flow.run_local_server(port=0)
+     with open('token.pickle', 'wb') as token:
+          pickle.dump(creds, token)
 
 service = build('sheets', 'v4', credentials=creds)
 
