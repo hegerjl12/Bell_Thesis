b0f8eba4bdade72c493e50d65df3d8460591c1b3
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-29 13:11:33 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -76,7 +76,8 @@ if st.session_state.i < 4:
 
 else:
 
-     text = " ".join(st.session_state.image_words[0])
+    # text = " ".join(st.session_state.image_words[0])
+     text = " ".join(WordDB.get("1"))
      word_cloud = WordCloud(collocations = False, background_color = 'white').generate(text)
      fig = plt.imshow(word_cloud, interpolation='bilinear')
      plt.axis("off")
