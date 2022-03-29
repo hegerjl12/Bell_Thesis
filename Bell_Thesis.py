b0280b58bea8364dcd685b2cff1300c61f29a929
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-29 14:54:27 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -94,8 +94,8 @@ else:
           image_words = item.get('words')
           st.write(image_words)
      
-     text = " ".join(st.session_state.image_words[0])
-     text = " ".join(image_words)
+     #text = " ".join(st.session_state.image_words[0])
+     text = " ".join(str(image_words))
      word_cloud = WordCloud(collocations = False, background_color = 'white').generate(text)
      fig = plt.imshow(word_cloud, interpolation='bilinear')
      plt.axis("off")
