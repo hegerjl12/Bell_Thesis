5641704f507dc7971be03c74efc08511688f90c9
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-29 11:12:01 -0700

    d

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -31,15 +31,15 @@ creds = None
 if os.path.exists('token.pickle'):
      with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-if not creds or not creds.valid:
-     if creds and creds.expired and creds.refresh_token:
-          creds.refresh(Request())
-     else:
-          flow = InstalledAppFlow.from_client_secrets_file(
-          'token.json', SCOPES) # here enter the name of your downloaded JSON file
-          creds = flow.run_local_server(port=0)
-     with open('token.pickle', 'wb') as token:
-          pickle.dump(creds, token)
+#if not creds or not creds.valid:
+ #    if creds and creds.expired and creds.refresh_token:
+  #        creds.refresh(Request())
+   #  else:
+    #      flow = InstalledAppFlow.from_client_secrets_file(
+     #     'token.json', SCOPES) # here enter the name of your downloaded JSON file
+     #     creds = flow.run_local_server(port=0)
+     #with open('token.pickle', 'wb') as token:
+      #    pickle.dump(creds, token)
 
 service = build('sheets', 'v4', credentials=creds)
 
