aeede26aaa5f2ea84b2f0e81ac8ce769fc18961b
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-30 16:39:19 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -24,14 +24,14 @@ if 'images_left' not in st.session_state:
 if 'i' not in st.session_state:
      st.session_state.i = 1
 
-if 'text1' not in st.session_state:
-     st.session_state.text1 = ""
+#if 'text1' not in st.session_state:
+  #   st.session_state.text1 = ""
 
-if 'text2' not in st.session_state:
-     st.session_state.text2 = ""
+#if 'text2' not in st.session_state:
+ #    st.session_state.text2 = ""
 
-if 'text3' not in st.session_state:
-     st.session_state.text3 = ""
+#if 'text3' not in st.session_state:
+ #    st.session_state.text3 = ""
 
 
 
@@ -45,34 +45,6 @@ def set_image():
 
           # remove last image
                st.session_state.images_left.remove(st.session_state.i)
-
-def connect_databases():
-     # connect to databases
-     with st.spinner("Connecting to database..."):
-          deta = Deta(st.secrets["deta_key"])
-          Image1DB = deta.Base("image1db")
-          Image2DB = deta.Base("image2db")
-          Image3DB = deta.Base("image3db")
-          Image4DB = deta.Base("image4db")
-          Image5DB = deta.Base("image5db")
-
-def commit_to_database():
-     if st.session_state.i == 1:
-          Image1DB.put({"words": st.session_state.text1})
-          Image1DB.put({"words": st.session_state.text2})
-          Image1DB.put({"words": st.session_state.text3})
-     if st.session_state.i == 2:
-          Image2DB.put({"words": st.session_state.text1})
-          Image2DB.put({"words": st.session_state.text2})
-          Image2DB.put({"words": st.session_state.text3})
-     if st.session_state.i == 3:
-          Image3DB.put({"words": st.session_state.text1})
-          Image3DB.put({"words": st.session_state.text2})
-          Image3DB.put({"words": st.session_state.text3})
-
-     st.write(st.session_state.text1+"?")
-
-connect_databases()
 
 with st.spinner("Connecting to database..."):
           deta = Deta(st.secrets["deta_key"])
@@ -89,12 +61,25 @@ if len(st.session_state.images_left) > 0:
      set_image() 
 
      form = st.form(key='wordForm'+str(st.session_state.i))
-     st.session_state.text1 = form.text_input('','', key=st.session_state.i+10)
-     st.session_state.text2 = form.text_input('','', key=st.session_state.i+20)
-     st.session_state.text3 = form.text_input('','', key=st.session_state.i+30)
-
+     text1 = form.text_input('','', key=st.session_state.i+10)
+     text2 = form.text_input('','', key=st.session_state.i+20)
+     text3 = form.text_input('','', key=st.session_state.i+30)
 
      submit = form.form_submit_button('Submit', on_click=commit_to_database)
+
+     if submit:
+          if st.session_state.i == 1:
+               Image1DB.put({"words": text1})
+               Image1DB.put({"words": text2})
+               Image1DB.put({"words": text3})
+          if st.session_state.i == 2:
+               Image2DB.put({"words": text1})
+               Image2DB.put({"words": text2})
+               Image2DB.put({"words": text3})
+          if st.session_state.i == 3:
+               Image3DB.put({"words": text1})
+               Image3DB.put({"words": text2})
+               Image3DB.put({"words": text3})
 
 else:
      st.write("Thank you!")
