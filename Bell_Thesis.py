17d37a8b17430a7d355cc515655a1c48a209d273
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-30 13:13:10 -0700

    test rules

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -64,7 +64,8 @@ if st.session_state.images_left:
      if submit:
           # commit input to database
           if st.session_state.i == 1:
-               Image1DB.put({"words": text1})
+               if len(text1) > 0 and text1.isspace():
+                    Image1DB.put({"words": text1.spit().pop(0)})
                Image1DB.put({"words": text2})
                Image1DB.put({"words": text3})
           if st.session_state.i == 2:
