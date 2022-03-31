d5ebdc88ac8a496bd64c9afbaddc305c2597e29d
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-03-30 17:38:06 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -47,11 +47,11 @@ def set_image():
           # remove last image
                st.session_state.images_left.remove(st.session_state.i)
 
-with st.spinner("Connecting to database..."):
-          deta = Deta(st.secrets["deta_key"])
-          Image1DB = deta.Base("testdb1")
-          Image2DB = deta.Base("testdb2")
-          Image3DB = deta.Base("testdb3")
+#with st.spinner("Connecting to database..."):
+          #deta = Deta(st.secrets["deta_key"])
+          #Image1DB = deta.Base("testdb1")
+         # Image2DB = deta.Base("testdb2")
+        #  Image3DB = deta.Base("testdb3")
          # Image4DB = deta.Base("image4db")
         #  Image5DB = deta.Base("image5db")
 
@@ -70,17 +70,26 @@ if len(st.session_state.images_left) > 0:
 
      if submit:
           if st.session_state.i == 1:
-               Image1DB.put({"words": text1})
-               Image1DB.put({"words": text2})
-               Image1DB.put({"words": text3})
+               st.write(text1)
+            #   Image1DB.put({"words": text1})
+               st.write(text2)
+             #  Image1DB.put({"words": text2})
+               st.write(text3)
+              # Image1DB.put({"words": text3})
           if st.session_state.i == 2:
-               Image2DB.put({"words": text1})
-               Image2DB.put({"words": text2})
-               Image2DB.put({"words": text3})
+               st.write(text1)
+              # Image2DB.put({"words": text1})
+               st.write(text2)
+              # Image2DB.put({"words": text2})
+               st.write(text3)
+              # Image2DB.put({"words": text3})
           if st.session_state.i == 3:
-               Image3DB.put({"words": text1})
-               Image3DB.put({"words": text2})
-               Image3DB.put({"words": text3})
+               st.write(text1)
+              # Image3DB.put({"words": text1})
+               st.write(text2)
+              # Image3DB.put({"words": text2})
+               st.write(text3)
+              # Image3DB.put({"words": text3})
 
 else:
      st.write("Thank you!")
