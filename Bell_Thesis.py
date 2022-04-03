8ebc5ade48eef2ac422458a1a367413d9880f749
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-04-03 10:36:09 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -44,14 +44,16 @@ if 'submitted' not in st.session_state:
 # set selected image to the image container
 def set_image():
      if len(st.session_state.images_left) > 0:
-          st.session_state.i = random.choice(st.session_state.images_left)
           image = Image.open('image' + str(st.session_state.i) + '.jpg')
           with image_container.container():
                st.image(image, width=360, use_column_width='auto')
+               st.write("Enter 3 words the image makes you feel: ")
 
           # remove last image
                st.session_state.last_image.append(st.session_state.i)
                st.session_state.images_left.remove(st.session_state.i)
+     else:
+          image_container.empty()
 
 def commit_words(text1, text2, text3):
 
@@ -92,29 +94,26 @@ with st.spinner("Connecting to database..."):
           Image4DB = deta.Base("image4db")
           Image5DB = deta.Base("image5db")
 
+# loop to print images and collect input
+#if len(st.session_state.images_left) > 0:
+
+#st.write("Enter 3 words the image makes you feel: ")
+set_image() 
+
+#  text = st.text_input(label='')
+#  commit_words(text, text, text, st.session_state.i)
+
+with st.form('entries', clear_on_submit=True):
+     st.session_state.text1 = st.text_input(label="",key=1)
+     st.session_state.text2 = st.text_input(label="",key=2)
+     st.session_state.text3 = st.text_input(label="",key=3)
+     
+     st.session_state.submitted = st.form_submit_button('Submit')
+
 if st.session_state.submitted:
-     if len(st.session_state.images_left) > 1:
-          commit_words(st.session_state.text1, st.session_state.text2, st.session_state.text3)
+     if len(st.session_state.images_left) > 0:
+          commit_words(t.session_state.text1, st.session_state.text2, st.session_state.text3)
      else:
           commit_wordsFinal(st.session_state.text1, st.session_state.text2, st.session_state.text3)
-# loop to print images and collect input
-if len(st.session_state.images_left) > 0:
-
-     st.write("Enter 3 words the image makes you feel: ")
-     set_image() 
-
-   #  text = st.text_input(label='')
-   #  commit_words(text, text, text, st.session_state.i)
-
-     with st.form('entries', clear_on_submit=True):
-          st.session_state.text1 = st.text_input(label="",key=1)
-          st.session_state.text2 = st.text_input(label="",key=2)
-          st.session_state.text3 = st.text_input(label="",key=3)
-          
-          st.session_state.submitted = st.form_submit_button('Submit')
-
-    
-
-else:
-     #commit_wordsFinal(st.session_state.text1, st.session_state.text2, st.session_state.text3)
-     st.write("Thank you!")
+          st.write("Thank you!")
+#else:     
