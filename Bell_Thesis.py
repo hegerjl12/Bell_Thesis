f2fd3c019d85da3a546bcdab65f5899889129e7f
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-04-02 20:46:01 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -74,6 +74,19 @@ if len(st.session_state.images_left) > 0:
 
           submit = st.form_submit_button('Submit')
 
+          if st.session_state.i == 1:
+               Image1DB.put({"words": text1})
+               Image1DB.put({"words": text2})
+               Image1DB.put({"words": text3})
+          if st.session_state.i == 2:
+               Image2DB.put({"words": text1})
+               Image2DB.put({"words": text2})
+               Image2DB.put({"words": text3})
+          if st.session_state.i == 3:
+               Image3DB.put({"words": text1})
+               Image3DB.put({"words": text2})
+               Image3DB.put({"words": text3})
+
      if submit:
 
           if st.session_state.i == 1:
