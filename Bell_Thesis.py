bb8bba9c995220511073e730d5d3458988854370
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-04-03 00:35:31 -0700

    !

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -97,6 +97,5 @@ if len(st.session_state.images_left) > 0:
           commit_words(st.session_state.text1, st.session_state.text2, st.session_state.text3)
          
 else:
-     if st.session_state.submitted:
-          commit_words(st.session_state.text1, st.session_state.text2, st.session_state.text3)
+     commit_words(st.session_state.text1, st.session_state.text2, st.session_state.text3)
      st.write("Thank you!")
