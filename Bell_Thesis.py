801c312692c4b68d64a2a23d56d65d1396028176
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-04-02 22:14:08 -0700

    t

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -80,7 +80,7 @@ if len(st.session_state.images_left) > 0:
           st.session_state.text2 = st.text_input(label='', key=st.session_state.i+20)
           st.session_state.text3 = st.text_input(label='', key=st.session_state.i+30)
 
-          submit = st.button('Submit', on_click=commit_words)
+          submit = st.form_submit_button('Submit', on_click=commit_words)
 
           st.write("did this happen?1")
 
