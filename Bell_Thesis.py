913827902947d4653466da72b2f415da1ebc471b
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-04-23 11:32:09 -0700

    fix

--- Bell_Thesis.py
+++ Bell_Thesis.py
@@ -125,5 +125,5 @@ if st.session_state.submitted:
      else:
           commit_wordsFinal(st.session_state.text1.lower(), st.session_state.text2.lower(), st.session_state.text3.lower())
           input_container.empty()
-          st.subheading("Thank you!  Check out the wordclouds to see impressions in real time.")
+          st.subheader("Thank you!  Check out the wordclouds to see impressions in real time.")
           st.stop()
